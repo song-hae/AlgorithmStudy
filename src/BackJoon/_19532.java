f8475c171b17b54e3b04c9b268ead85be331e61f
songhae <boricocke@naver.com>
2025-01-22 17:37:04 +0900

    완전탐색(Brute Force) 브론즈 2

---
+++ src/BackJoon/_19532.java
@@ -0,0 +1,31 @@
+package BackJoon;
+
+import java.util.Scanner;
+
+public class _19532 {
+
+    public static void main(String[] args) {
+
+        //1. 연립 방정식이기에 and 연산 활용
+        //2. x,y는 -999부터 999까지의 정수이기에 순회하면서 만족하는 x,y찾기
+
+        // O(N^2)인데 2000*2000 이라 ㄱㅊ
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int f = sc.nextInt();
+
+        for(int i=-999; i<1000 ; i++){ // x 값
+            for(int j=-999; j<1000 ; j++){ // y 값
+                if((a*i + b*j == c) && (d*i + e*j == f)) { // 연립 방정식 성립 시
+                    System.out.print(i+" "+j);
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ src/BackJoon/_22864.java
@@ -0,0 +1,36 @@
+package BackJoon;
+
+import java.util.Scanner;
+
+public class _22864 {
+
+    public static void main(String[] args) {
+
+        /* 1.
+        A: 단위 피로도
+        B: 단위 작업량 -> 이걸 누적으로 더해서 리턴
+        C: 감소 피로도
+        M: 최대 피로도
+        2. A와 C는 한 변수에 접근, 그 변수를 M이 넘지 않도록, 반복은 24번,
+         */
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int m = sc.nextInt();
+        int t = 0; //현재 피로도
+        int ret = 0; //최대 작업량(리턴 값)
+
+        for(int i = 0; i < 24 ; i++){
+            if(t+a <= m){ // 작업 후 피로도가 최대 피로도보다 낮으면
+                t += a; // 작업
+                ret += b; // 작업량 증가
+            } else { // 아니면
+                t -= c; // 휴식
+                if(t < 0) t=0; // 휴식 후 음수면 0으로
+            }
+        }
+        System.out.println(ret);
+    }
+}
